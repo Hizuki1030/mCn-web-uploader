41396bce4d289a1f38317c047aac7816899eb164
aibahizuki <45566778+Hizuki1030@users.noreply.github.com>
2022-12-06 22:08:26 +0900

    update

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ app = Flask(__name__, static_folder='.', static_url_path='')
 def index():
     return app.send_static_file('index.html')
 
+
+@app.route('/LatestFirmwareUploader')
+def page1():
+    # 変数展開(messageとnameの値がHTMLに渡される)
+    return app.send_static_file('LatestFirmwareUploader.html')
+
+
 app.run(port=8000, debug=True)
